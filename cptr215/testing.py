656826b64c23c09e0eccb4bbcc20ce2edf1abc5f
Kellenerskine <kellenae129@gmail.com>
2021-09-06 10:31:33 -0400

    added the python unit tests examples

--- cptr215/testing.py
+++ cptr215/testing.py
@@ -1,3 +1,17 @@
+def plural_of(word):
+    """Returns the plural of the word.
+    >>> plural_of("word")
+    'words'
+    >>> plural_of("penny")
+    'pennies'
+    """
+
+    if word.endswith("y"):
+        return word[:-1] + "ies"
+    else:
+        return word + "s"
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
