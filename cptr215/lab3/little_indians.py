e2c2dbee281faebac29c766e7b7f45ee1c2134fc
Kellenerskine <kellenae129@gmail.com>
2021-09-09 18:01:12 -0400

    warmup done

---
+++ cptr215/lab3/little_indians.py
@@ -0,0 +1,14 @@
+n = input()
+if n == '':
+    n = 10
+else:
+    n = int(n)
+
+for i in range(1, n):
+    print(i, "little", end="")
+    if (i % 3) == 0:
+        print(" Indians;")
+    else:
+        print(", ", end="")
+
+print("{} little Indian boys.".format(n))
