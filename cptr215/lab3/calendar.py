8e94c2db1d504f17c1cfb426643bc7551fc12347
Kellenerskine <kellenae129@gmail.com>
2021-09-14 20:56:19 -0400

    frustrated

--- cptr215/lab3/calendar.py
+++ cptr215/lab3/calendar.py
@@ -1,16 +1,16 @@
-def day_of_week_num(year, month, day):
-    if month == 1:
-        month = 13
-        year -= 1
+def day_of_week_num(user_year, user_month):
+    if user_month == 1:
+        user_month = 13
+        user_year -= 1
 
-    if month == 2:
-        month = 14
-        year -= 1
+    if user_month == 2:
+        user_month = 14
+        user_year -= 1
 
-    q = day
-    m = month
-    k = year % 100
-    j = year // 100
+    q = 1 # day
+    m = user_month
+    k = user_year % 100
+    j = user_year // 100
     h = q + 13 * (m + 1) // 5 + k + k // 4 + j // 4 + 5 * j
     result = (h % 7)
     return result
@@ -51,14 +51,29 @@ def leap_year(year):
         return False
 
 
-#def starting_day()
+def num_days():
+    # some code here to determine the number of day in a month
+
 
 
 user_input = input("Input the month and year: ").split()
 user_month = int(user_input[0])
 user_year = int(user_input[1])
 
+start_day_num = day_of_week_num(user_year, user_month)
+month_start_day = day_of_week_word(start_day_num)
+
 month_name_list = ("", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 
 print("      ", month_name_list[user_month])
 print("Su", "Mo", "Tu", "We", "Th", "Fr", "Sa")
+for day in range(user_month):
+    print(day, '')
+
+
+
+# objective
+# 1. find number of days in a month
+#         - is leap year?
+#         - is feb?
+# 2. print the first of the month under the correct day with spaces as a string which then mulipies by the amount of days before the start day.
