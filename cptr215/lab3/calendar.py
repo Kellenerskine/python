cd6755147d4c0405f5106ae2a0b7f14bc85e4b47
Kellenerskine <kellenae129@gmail.com>
2021-09-16 15:03:14 -0400

    just need to fix whitespace errors and write tests

--- cptr215/lab3/calendar.py
+++ cptr215/lab3/calendar.py
@@ -1,16 +1,16 @@
-def day_of_week_num(user_year, user_month):
-    if user_month == 1:
-        user_month = 13
-        user_year -= 1
+def day_of_week_num(user_year2, user_month2):
+    if user_month2 == 1:
+        user_month2 = 13
+        user_year2 -= 1
 
-    if user_month == 2:
-        user_month = 14
-        user_year -= 1
+    if user_month2 == 2:
+        user_month2 = 14
+        user_year2 -= 1
 
-    q = 1 # day
-    m = user_month
-    k = user_year % 100
-    j = user_year // 100
+    q = 1  # day
+    m = user_month2
+    k = user_year2 % 100
+    j = user_year2 // 100
     h = q + 13 * (m + 1) // 5 + k + k // 4 + j // 4 + 5 * j
     result = (h % 7)
     return result
@@ -31,7 +31,7 @@ def day_of_week_word(day_num):
     return day_of_week
 
 
-def leap_year(year):
+def leap_year_check(year):
     is_leap_year = False
 
     input_year = year
@@ -51,29 +51,61 @@ def leap_year(year):
         return False
 
 
-def num_days():
-    # some code here to determine the number of day in a month
-
-
-
-user_input = input("Input the month and year: ").split()
-user_month = int(user_input[0])
-user_year = int(user_input[1])
+user_input = input("Input the month and year: ").split()  # bring in month and year
+user_month = int(user_input[0])  # user inputted month
+user_year = int(user_input[1])  # user inputted year
 
 start_day_num = day_of_week_num(user_year, user_month)
 month_start_day = day_of_week_word(start_day_num)
+num_days_in_month = 0
+num_days_dict = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 30,
+        9: 31,
+        10: 30,
+        11: 31,
+        12: 30
+    }
 
-month_name_list = ("", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
-
-print("      ", month_name_list[user_month])
-print("Su", "Mo", "Tu", "We", "Th", "Fr", "Sa")
-for day in range(user_month):
-    print(day, '')
+if leap_year_check(user_year) and user_month == 2:  # finds how many days in month if leap year
+    num_days_in_month = 29
+else:
+    num_days_in_month = num_days_dict[user_month]
 
 
+month_name_list = (
+    "", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+    "December")
+
+# the generic header text for the calendar
+month_name_out = month_name_list[user_month]
+header = (month_name_out + " " + str(user_year))
+# gets the month name
+print(str(header).center(20))  # centers the month name
+print("Su", "Mo", "Tu", "We", "Th", "Fr", "Sa")  # prints the day names
+
+# the loop to print the calendar days
+count = 7 - start_day_num
+for day in range(1, num_days_in_month+1):
+    if day == 1:
+        spaces = " " * (start_day_num*3)
+        print(spaces, end='')
+    if day > 9:
+        print(day, "", end='')
+    else:
+        print(day, ' ', end='')
+    if (count % 7 == 0) and count != 0:
+        print()
+    count += 1
 
 # objective
-# 1. find number of days in a month
-#         - is leap year?
-#         - is feb?
-# 2. print the first of the month under the correct day with spaces as a string which then mulipies by the amount of days before the start day.
+# 1. find the first day of the month
+# 2. print the first of the month under the correct day with spaces as a
+# string which then multiplies by the amount of days before the start day.
+# 3. write tests
