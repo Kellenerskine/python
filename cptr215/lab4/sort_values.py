80d732150ec222755fd5d137d523159350864ec2
Kellenerskine <kellenae129@gmail.com>
2021-09-16 18:27:38 -0400

    no progress rly

--- cptr215/lab4/sort_values.py
+++ cptr215/lab4/sort_values.py
@@ -1,1 +1,17 @@
-def isStrictlyIncreasing()
+def isStrictlyIncreasing(liste):
+    listee = list(liste)
+    print(listee)
+    maxi = -200000
+    for i in listee:
+        if i > maxi:
+            maxi = i
+        else:
+            return False
+    return True
+
+
+liste = "almost"
+if isStrictlyIncreasing(liste):
+    print("true")
+else:
+    print("false")
