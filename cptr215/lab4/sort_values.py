27130a7423e8b84141b93343d94871cdb53d0641
Kellenerskine <kellenae129@gmail.com>
2021-09-20 11:13:57 -0400

    kinda stuck, kinda not

--- cptr215/lab4/sort_values.py
+++ cptr215/lab4/sort_values.py
@@ -1,17 +1,26 @@
-def isStrictlyIncreasing(liste):
-    listee = list(liste)
-    print(listee)
-    maxi = -200000
-    for i in listee:
-        if i > maxi:
-            maxi = i
+def isStrictlyIncreasing(input_list):
+    # """should take in any input and return whether it is incresaing or not
+    # >>> isStrictlyIncreasing([1, 3, 5, 7, 9])
+    # True
+    # >>> isStrictlyIncreasing("almost")
+    # True
+    # >>> isStrictlyIncreasing([4, 3, 8, 1, 0])
+    # False
+    # """
+    # values = []
+    # for i in input_list:
+    #     values.append(ord(str(i)))
+
+    for i in input_list:
+        if i > i + 1:
+            pass
         else:
             return False
     return True
 
 
-liste = "almost"
-if isStrictlyIncreasing(liste):
+input_list = [45, 45, 90]
+if isStrictlyIncreasing(input_list):
     print("true")
 else:
     print("false")
