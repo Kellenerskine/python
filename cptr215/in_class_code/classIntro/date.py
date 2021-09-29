0b0d2e18aa6a3ab9cd19c299ee152263f7d1cd02
Kellenerskine <kellenae129@gmail.com>
2021-09-29 10:49:40 -0400

    some work on the date.py no longer works

--- cptr215/in_class_code/classIntro/date.py
+++ cptr215/in_class_code/classIntro/date.py
@@ -31,16 +31,42 @@ class Date:
         # i wrote the above zeller form wrong, program therefore does not work.
         return 7 if dow == 0 else dow
 
-    def print(self):
+    def __str__(self):
         """Outputs self in MMM d, yyy format.
-        >>> Date(2021, 9, 27)
-        Sep 27, 2021
-        >>> independence = Date(1776, 7, 4)
-        >>> independence.print()
+        >>> Date(2000, 1, 1).__str__() # not common
+        'Jan 1, 2000'
+        >>> str(Date(2021, 9, 27))
+        'Sep 27, 2021'
+        >>> independence = str(Date(1776, 7, 4))
+        >>> print(independence)
         Jul 4, 1776
         """
         month_name = "not Jan Feb Mar Apr May Jun Aug Sep Oct Nov Dec".split()[self.month]
         print(f"{month_name} {self.day}, {self.year}")
+
+    def __repr__(self):
+        """Returns a string that would evaluate to an identical Date object
+        >>> Date(2021, 9, 29).__repr__() #still not common
+        'Date(2021, 9, 29)'
+        >>> Date(1970, 12, 31)
+        Date(1970, 12, 31)
+        >>> repr(Date(1984, 2, 20))
+        'Date(1984, 2, 20)'
+        """
+        return f"Date({self.year}, {self.month}, {self.day})"
+
+    def is_leap_year(self):
+    """determines whether self is in a leap year
+    >>> Date(2021, 9, 29).is_leap_year()
+    False
+    >>> Date(1984, 4, 27).is_leap_year()
+    True
+    >>> Date(2000, 1, 1).is_leap_year()
+    True
+    >>> Date(1900, 11, 30).is_leap_year()
+    False
+    """
+    return self.year %
 
 
 if __name__ == '__main__':
