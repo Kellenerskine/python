feb4987b2cb752f9f3c3a09eee9f48e0e2c2d5b4
Kellen Erskine <kellenae129@gmail.com>
2021-10-12 09:54:07 -0400

    worked on set

--- cptr215/lab5/lab/set_game.py
+++ cptr215/lab5/lab/set_game.py
@@ -48,9 +48,18 @@ class SetCard:
 
     def third_card(self, other):
         """Returns the third card that makes a set with self and other.
+        #objectives:
+        #1.compare all attributes of the cards
+        #2.find a card that matches a specific atribute that both cards share
+        #3.output that card using repr
+        #4.check when there is no equivalent card
         >>> card1 = SetCard(2, Fill.EMPTY, Color.RED, Shape.QUAD)
         >>> card2 = SetCard(1, Fill.SHADED, Color.BLUE, Shape.OVAL)
         >>> card3 = SetCard(2 ,Fill.FILLED, Color.GREEN, Shape.PYRAMID)
+        >>> card4 = SetCard(1, Fill.FILLED, Color.Red, Shape.Quad)
+        >>> card5 = SetCard(2, Fill.SHADED, Color.BLUE, Shape.PYRAMID)
+        >>> print(card4.third_card(card5))
+        3EGO
         >>> print(card1.third_card(card2))
         3FGP
         >>> print(card2.third_card(card1))
@@ -58,21 +67,39 @@ class SetCard:
         >>> print(card3.third_card(card1))
         2SBO
         """
+        #
+        # fill = 0
+        # color = 0
+        # shape = 0
+        #
+        #
+        # for
+        # if self.color == other.color
 
-        card1 = str(self)
-        card2 = str(other)
+        first_card = str(self)
+        second_card = str(other)
+        result = ""
 
-        for j in card1:
-            for k in card2:
+        for j in first_card:
+            for k in second_card:
                 if j == k:
-
                     print(j, k)
+                    result = result + j
+                    print(result)
 
 
 def make_deck():
-    '''Returns a list containing a complete Set deck with 81 unique cards.'''
+    """Returns a list containing a complete Set deck with 81 unique cards."""
 
 
 def is_set(card1, card2, card3):
-    '''Determines whether the 3 cards make a set.
-    (For each of the 4 traits, all 3 cards are either the same, or all 3 are different.)'''
+    """Determines whether the 3 cards make a set.
+    (For each of the 4 traits, all 3 cards are either the same, or all 3 are different.)
+    1.call third card on two of the cards, check if the third on equals the thirds card given
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
