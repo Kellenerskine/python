350971763e3c062be2d357cd41f51a20dd124830
Kellenerskine <kellenae129@gmail.com>
2021-10-25 11:04:56 -0400

    started on inheretance with the counter classes

--- cptr215/in_class_code/inheritanceIntro/counter.py
+++ cptr215/in_class_code/inheritanceIntro/counter.py
@@ -6,6 +6,7 @@ Prof. O & CPTR-215
 2021-10-01 first draft
 """
 
+
 class BoundedCounter:
     def __init__(self, lower_bound, upper_bound, neighbor=None):
         """
@@ -27,6 +28,7 @@ class BoundedCounter:
         self.upper_bound = upper_bound
         self.current_value = self.lower_bound
         self.neighbor = neighbor
+
     def increment(self):
         """
         >>> digit = BoundedCounter(0, 9)
@@ -46,21 +48,24 @@ class BoundedCounter:
                 self.neighbor.increment()
         else:
             self.current_value += 1
+
     def get_value(self):
         return self.current_value
-    def __str__(self):
-        return (str(self.neighbor) if self.neighbor \
-            else "") + str(self.current_value)
 
-class ListCounter:
+    def __str__(self):
+        return (str(self.neighbor) if self.neighbor else "") + str(self.current_value)
+
+
+class ListCounter(BoundedCounter):
     def __init__(self, items, neighbor=None):
         self.items = tuple(items)
-        self.index = BoundedCounter(0, len(self.items) - 1, neighbor)
-    def increment(self):
-        self.index.increment()
+        BoundedCounter.__init__(self, 0, len(self.items) - 1, neighbor)
+
     def get_value(self):
-        return self.items[self.index.get_value()]
+        return self.items[self.current_value]
+
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
