a422683fa157db662350ab3248a0af69a3461094
Kellenerskine <kellenae129@gmail.com>
2021-11-04 17:06:24 -0400

    ls almost done

--- cptr215/lab9/warmup/ls.py
+++ cptr215/lab9/warmup/ls.py
@@ -1,2 +1,17 @@
 import sys
 import os
+
+working_dir = os.getcwd()
+
+result_list = []
+
+for dirname, subdirs, files in os.walk(working_dir):
+    for i in subdirs:
+        result_list.append(i + "/")
+    for file in files:
+        result_list.append(file)
+
+result_list = sorted(result_list, key=str.lower)
+for i in result_list:
+    print(i)
+
