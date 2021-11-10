aca0a86ddf89e00fcd2fd398726e7ffce4b0bc00
Kellenerskine <kellenae129@gmail.com>
2021-11-10 11:50:36 -0500

    worked on fgrep

--- cptr215/lab9/warmup/fgrep.py
+++ cptr215/lab9/warmup/fgrep.py
@@ -1,12 +1,19 @@
 import sys
 
-i = 1
-word = sys.argv[1]
+search_word = "" + sys.argv[2]
+results = ""
+i = 3
 
 while i < len(sys.argv):
-    for line in sys.argv[i]:
-        pass
-
+    current_file = sys.argv[i]
+    with open(current_file) as file:
+        for lines in file:
+            line = lines.split()
+            for word in line:
+                if word == search_word:
+                    results += lines
 
 
     i += 1
+
+print(results)
