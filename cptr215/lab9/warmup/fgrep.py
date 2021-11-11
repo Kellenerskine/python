15c8cceb7a17741ca064cc6a1ad90386fee63b7f
Kellenerskine <kellenae129@gmail.com>
2021-11-11 15:43:08 -0500

    done with fgrep? still need to test it before showing profo

--- cptr215/lab9/warmup/fgrep.py
+++ cptr215/lab9/warmup/fgrep.py
@@ -1,19 +1,42 @@
 import sys
 
-search_word = "" + sys.argv[2]
+search_word = sys.argv[1]
 results = ""
-i = 3
+i = 2
+line_num = 0
+count_lines = False
+ignore_case = False
+
+for k in sys.argv:
+    if k == "-i":
+        sys.argv.remove(k)
+        ignore_case = True
+
+for j in sys.argv:
+    if j == "-n":
+        sys.argv.remove(j)
+        count_lines = True
 
 while i < len(sys.argv):
     current_file = sys.argv[i]
     with open(current_file) as file:
         for lines in file:
-            line = lines.split()
-            for word in line:
-                if word == search_word:
+            line_num += 1
+            if ignore_case:
+                if search_word.lower() in lines.lower() or search_word in lines:
+                    results += lines
+            elif not ignore_case:
+                if search_word in lines:
                     results += lines
 
+        if len(sys.argv) > 2 and count_lines == False:
+            print(f"{current_file} {results}")
+        elif len(sys.argv) <= 2 and count_lines == False:
+            print(f"{results}")
+        elif len(sys.argv) > 2 and count_lines == True:
+            print(f"Line {line_num} {current_file} {results}")
+        elif len(sys.argv) <= 2 and count_lines == True:
+            print(f"Line {line_num} {results}")
 
+        results = ""
     i += 1
-
-print(results)
