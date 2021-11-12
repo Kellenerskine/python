a7cd9fa3ad6feaf3e218b89d9f8d8d2efa65e4a1
Kellen Erskine <kellenae129@gmail.com>
2021-11-12 03:17:17 -0500

    some progress with the warmup

--- cptr215/lab10/warmup/shopping_gui.py
+++ cptr215/lab10/warmup/shopping_gui.py
@@ -1,30 +1,42 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QListView
+from PySide6.QtWidgets import QApplication, QListWidgetItem, QMainWindow, QLineEdit, QVBoxLayout, QGroupBox, \
+    QHBoxLayout, QWidget, \
+    QListWidget, QListWidgetItem, QListView, QGridLayout, QPushButton
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Shopping list")
-        self.input = QLineEdit()
-        self.input.setFixedWidth(200)
         self.setFixedSize(400, 600)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
+        self.input = QLineEdit()
+        self.input.setFixedWidth(300)
+
+        self.my_list = QListWidget()
+
+        add_item = QPushButton("Add")
+        add_item.clicked.connect(self.addListItem())
+
+        layout = QGridLayout()
+        layout.setColumnStretch(1, 4)
+        layout.setColumnStretch(2, 4)
+        layout.addWidget(self.input, 0, 0)
+        layout.addWidget(add_item, 0, 3)
 
         container = QWidget()
         container.setLayout(layout)
-
         self.setCentralWidget(container)
 
     def addListItem(self):
-        items = [4, 5, 7]
-        listWidget = QListWidget(self)
-        listWidget.show()
-        listWidget.addItem(items)
+        # takes content of box
+        item = self.input.text()
+        self.my_list.addItem(item)
+        self.input.setText("")
+        print("clicked")
 
-
+    def removeListItem(self):
+        pass
 
 
 app = QApplication(sys.argv)
