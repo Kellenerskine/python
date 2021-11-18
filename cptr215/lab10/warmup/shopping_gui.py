b0650e8e0fcb939c1406c5e1c9b567dfe15555c6
Kellen Erskine <kellenae129@gmail.com>
2021-11-18 13:20:25 -0500

    need to make the shopping list save to a file

--- cptr215/lab10/warmup/shopping_gui.py
+++ cptr215/lab10/warmup/shopping_gui.py
@@ -1,4 +1,5 @@
 import sys
+from PySide6 import QtWidgets, QtCore
 from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QStyle, QLabel, QListWidgetItem, QMainWindow, QLineEdit, QVBoxLayout, \
     QGroupBox, \
@@ -16,21 +17,27 @@ class MainWindow(QMainWindow):
         self.input.setFixedWidth(300)
 
         self.my_list = QListWidget()
+        self.my_list.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.my_list.setSortingEnabled(True)
 
         add_item = QPushButton("Add")
         add_item.clicked.connect(self.addListItem)
 
         remove_item = QPushButton("Remove Selected Items")
-        remove_item.clicked.connect(self.removeListItem())
+        remove_item.clicked.connect(self.removeListItem)
 
         self.text = QLabel(text="Shopping List")
         self.text.setIndent(150)
         self.text.setFont(QFont("Times", weight=QFont.Bold))
         self.text.show()
 
+        #TODO: TEST
+        app.aboutToQuit.connect(self.closeEvent)
+
         layout = QGridLayout()
         layout.addWidget(self.text, 0, 0)
         layout.addWidget(self.input, 1, 0)
+        layout.addWidget(remove_item, 5, 0)
         layout.addWidget(add_item, 1, 1)
 
         layout.addWidget(self.my_list, 3, 0)
@@ -46,7 +53,16 @@ class MainWindow(QMainWindow):
         self.input.setText("")
 
     def removeListItem(self):
-        pass
+        list_items = self.my_list.selectedItems()
+        if not list_items: return
+        for item in list_items:
+            self.my_list.takeItem(self.my_list.row(item))
+
+    # def closeEvent(self):
+    #     #Your desired functionality here
+    #     print('Close button pressed')
+    #     import sys
+    #     sys.exit(0)
 
 
 app = QApplication(sys.argv)
@@ -57,3 +73,8 @@ window.show()
 app.exec()
 
 # TODO: focus cursor on text box for next item
+# TODO: sort the items by alph - done
+# TODO: enable multiselect - done
+# TODO: remove items that are selected with button
+# TODO: require a popup confirmation to exit app
+# TODO: save all changes to a file
