ed004b1a401b37559350123b4bb69d71bac0b221
Kellen Erskine <kellenae129@gmail.com>
2021-12-02 09:47:50 -0500

    counter is buggy

--- cptr215/lab12/sort_user_ids.py
+++ cptr215/lab12/sort_user_ids.py
@@ -1,40 +1,13 @@
 # Global variable
 num_calls = 0
 
-# # TODO: Write the partitioning algorithm - pick the middle element as the
-# #       pivot, compare the values using two index variables l and h (low and high),
-# #       initialized to the left and right sides of the current elements being sorted,
-# #       and determine if a swap is necessary
-# def partition(user_ids, low, high):
-#     global num_calls
-#     i_index = (low-1)
-#     pivot = user_ids[high]
-#     for j_index in range(low , high):
-#         if user_ids[j_index] <= pivot:
-#             i_index = i_index+1
-#         user_ids[i_index],user_ids[j_index] = user_ids[j_index],user_ids[i_index]
-#     user_ids[i_index+1],user_ids[high] = user_ids[high], user_ids[i_index+1]
-#     return (i_index+1)
-#
-# # TODO: Write the quicksort algorithm that recursively sorts the low and
-# #       high partitions. Add 1 to num_calls each time quisksort() is called
-# def quicksort(user_ids, i, k):
-#     global num_calls
-#     num_calls = num_calls + 1
-#     if i < k:
-#         pi = partition(user_ids,i,k)
-#         quicksort(user_ids, i , pi-1)
-#         quicksort(user_ids, pi+1, k)
-
-
-
-
 
 def sort(user_ids):
     less = []
     equal = []
     greater = []
     global num_calls
+    num_calls += 1
 
     if len(user_ids) > 1:
         pivot = user_ids[0]
@@ -46,13 +19,9 @@ def sort(user_ids):
             elif x < pivot:
                 greater.append(x)
         result = sort(greater) + equal + sort(less)
-        num_calls += 1
         return result
     else:
-        num_calls += 1
         return user_ids
-
-
 
 
 if __name__ == "__main__":
@@ -64,8 +33,6 @@ if __name__ == "__main__":
 
     # Initial call to sort
     user_ids = sort(user_ids)
-    num_calls += 1
-
 
     # Print number of calls to sort
     print(num_calls)
