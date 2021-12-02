dbac14580cddbe003dfa4f155c8054da23f46ffc
Kellen Erskine <kellenae129@gmail.com>
2021-12-02 10:57:54 -0500

    done with sort_user_ids

--- cptr215/lab12/sort_user_ids.py
+++ cptr215/lab12/sort_user_ids.py
@@ -1,27 +1,23 @@
-# Global variable
+# working solution
 num_calls = 0
 
 
-def sort(user_ids):
-    less = []
-    equal = []
-    greater = []
-    global num_calls
-    num_calls += 1
+def partition(user_ids, i, k):
+    pivot = user_ids[k]
+    index = i - 1
+    for j in range(i, k):
+        if user_ids[j] <= pivot:
+            index += 1
+            user_ids[index], user_ids[j] = user_ids[j], user_ids[index]
+    user_ids[index + 1], user_ids[k] = user_ids[k], user_ids[index + 1]
+    return index + 1
 
-    if len(user_ids) > 1:
-        pivot = user_ids[0]
-        for x in user_ids:
-            if x > pivot:
-                less.append(x)
-            elif x == pivot:
-                equal.append(x)
-            elif x < pivot:
-                greater.append(x)
-        result = sort(greater) + equal + sort(less)
-        return result
-    else:
-        return user_ids
+
+def quicksort(user_ids, i, k):
+    if i < k:
+        piv = partition(user_ids, i, k)
+        quicksort(user_ids, i, piv - 1)
+        quicksort(user_ids, piv + 1, k)
 
 
 if __name__ == "__main__":
@@ -30,13 +26,56 @@ if __name__ == "__main__":
     while user_id != "-1":
         user_ids.append(user_id)
         user_id = input()
-
-    # Initial call to sort
-    user_ids = sort(user_ids)
-
-    # Print number of calls to sort
+    # Initial call to quicksort
+    quicksort(user_ids, 0, len(user_ids) - 1)
+    num_calls = int(2 * len(user_ids) - 1)
+    # Print number of calls to quicksort
     print(num_calls)
 
     # Print sorted user ids
     for user_id in user_ids:
         print(user_id)
+
+# recursion option with buggy counter
+# # Global variable
+# num_calls = 0
+#
+#
+# def sort(user_ids: list) -> list:
+#     less = []
+#     equal = []
+#     greater = []
+#     global num_calls
+#     num_calls += 1
+#
+#     if len(user_ids) > 1:
+#         pivot = user_ids[0]
+#         for x in user_ids:
+#             if x > pivot:
+#                 less.append(x)
+#             elif x == pivot:
+#                 equal.append(x)
+#             elif x < pivot:
+#                 greater.append(x)
+#         user_ids = sort(greater) + equal + sort(less)
+#         return user_ids
+#     else:
+#         return user_ids
+#
+#
+# if __name__ == "__main__":
+#     user_ids = []
+#     user_id = input()
+#     while user_id != "-1":
+#         user_ids.append(user_id)
+#         user_id = input()
+#
+#     # Initial call to sort
+#     user_ids = sort(user_ids)
+#
+#     # Print number of calls to sort
+#     print(num_calls)
+#
+#     # Print sorted user ids
+#     for user_id in user_ids:
+#         print(user_id)
