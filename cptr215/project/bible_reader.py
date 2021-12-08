85ccf56e08aaf993a6e1bcb0134d8107b2c213a3
Kellen Erskine <kellenae129@gmail.com>
2021-12-08 17:59:54 -0500

    mostly working MVP

--- cptr215/project/bible_reader.py
+++ cptr215/project/bible_reader.py
@@ -7,16 +7,22 @@ from bs4 import BeautifulSoup
 
 # QApplication, QLabel, QMainWindow, QLineEdit, QWidget, QListWidget, QGridLayout, QPushButton
 
+# TODO: add chapter size to books in dict
+# TODO: grab values from drop downs and edit URL accordingly
+# TODO: make the GUI more attractive
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.book_chosen = ""
+        self.chapter_chosen = ""
+        self.version_chosen = ""
+        self.num_times = 0
+
         # sets a window of fixed size for the app
         self.setWindowTitle("Bible Statistics")
-        self.setFixedSize(400, 600)
-
-        # TODO: add dropdown boxes for version, book, and chapter
+        self.setFixedSize(500, 300)
 
         # creates a textbox that can be typed in
         self.input = QLineEdit()
@@ -25,7 +31,7 @@ class MainWindow(QMainWindow):
         search = QPushButton("Search")
         search.clicked.connect(self.search_bible)
 
-        stats = "random stuff"
+        # stats = "random stuff"
 
         # this section of code creates a title for the app and makes the app visible
         self.text = QLabel(text="Bible Statistics")
@@ -37,90 +43,94 @@ class MainWindow(QMainWindow):
         self.book_selector = QLabel(text="Books: ")
         self.chapter_selector = QLabel(text="Chapters: ")
         self.search_word = QLabel(text="Search word here: ")
-        self.filler = QLabel(text="")
-        self.stats = QLabel(text=stats)
+        self.filler = QLabel(text="Number of times the word appears: ")
+
+        # self.stats = QLabel(text=stats)
         # adds the version selector dropdown
         self.version = QComboBox()
         self.version.addItems(["NIV", "KJV", "NKJV", "MSG"])
-        self.version.currentIndexChanged.connect(self.version_select)
+        # self.version.currentIndexChanged.connect(self.version_select)
 
         # adds the book selector drop down
+        books = {
+            "Genesis": 50,
+            "Exodus": 40,
+            "Leviticus": 27,
+            "Numbers": 36,
+            "Deuteronomy": 34,
+            "Joshua": 24,
+            "Judges": 21,
+            "Ruth": 4,
+            "1 Samuel": 31,
+            "2 Samuel": 24,
+            "1 Kings": 22,
+            "2 Kings": 25,
+            "1 Chronicles": 29,
+            "2 Chronicles": 36,
+            "Ezra": 10,
+            "Nehemiah": 13,
+            "Esther": 10,
+            "Job": 42,
+            "Psalm": 150,
+            "Proverbs": 31,
+            "Ecclesiastes": 12,
+            "Song of Songs": 8,
+            "Isaiah": 66,
+            "Jeremiah": 52,
+            "Lamentations": 5,
+            "Ezekiel": 48,
+            "Daniel": 12,
+            "Hosea": 14,
+            "Joel": 3,
+            "Amos": 9,
+            "Obadiah": 1,
+            "Jonah": 4,
+            "Micah": 7,
+            "Nahum": 3,
+            "Habakkuk": 3,
+            "Zephaniah": 3,
+            "Haggai": 2,
+            "Zechariah": 14,
+            "Malachi": 4,
+            "Matthew": 28,
+            "Mark": 16,
+            "Luke": 24,
+            "John": 21,
+            "Acts": 28,
+            "Romans": 28,
+            "1 Corinthians": 16,
+            "2 Corinthians": 13,
+            "Galatians": 6,
+            "Ephesians": 6,
+            "Philippians": 4,
+            "Colossians": 4,
+            "1 Thessalonians": 5,
+            "2 Thessalonians": 3,
+            "1 Timothy": 6,
+            "2 Timothy": 4,
+            "Titus": 3,
+            "Philemon": 1,
+            "Hebrews": 13,
+            "James": 5,
+            "1 Peter": 5,
+            "2 Peter": 3,
+            "1 John": 5,
+            "2 John": 1,
+            "3 John": 1,
+            "Jude": 1,
+            "Revelation": 22
+        }
+
         self.book = QComboBox()
-        self.book.addItems([
-            "Genesis",
-            "Exodus",
-            "Leviticus",
-            "Numbers",
-            "Deuteronomy",
-            "Joshua",
-            "Judges",
-            "Ruth",
-            "1 Samuel",
-            "2 Samuel",
-            "1 Kings",
-            "2 Kings",
-            "1 Chronicles",
-            "2 Chronicles",
-            "Ezra",
-            "Nehemiah",
-            "Esther",
-            "Job",
-            "Psalms",
-            "Proverbs",
-            "Ecclesiastes",
-            "Song of Songs",
-            "Isaiah",
-            "Jeremiah",
-            "Lamentations",
-            "Ezekiel",
-            "Daniel",
-            "Hosea",
-            "Joel",
-            "Amos",
-            "Obadiah",
-            "Jonah",
-            "Micah",
-            "Nahum",
-            "Habakkuk",
-            "Zephaniah",
-            "Haggai",
-            "Zechariah",
-            "Malachi",
-            "Matthew",
-            "Mark",
-            "Luke",
-            "John",
-            "Acts",
-            "Romans",
-            "1 Corinthians",
-            "2 Corinthians",
-            "Galatians",
-            "Ephesians",
-            "Philippians",
-            "Colossians",
-            "1 Thessalonians",
-            "2 Thessalonians",
-            "1 Timothy",
-            "2 Timothy",
-            "Titus",
-            "Philemon",
-            "Hebrews",
-            "James",
-            "1 Peter",
-            "2 Peter",
-            "1 John",
-            "2 John",
-            "3 John",
-            "Jude",
-            "Revelation"
-        ])
-        self.book.currentIndexChanged.connect(self.book_select)
+        for i in books.keys():
+            self.book.addItem(i)
+        print(self.book)
 
         # adds the chapter selector drop down
         self.chapter = QComboBox()
         # TODO: add a loop to populate the dropdown with proper amount of chapters
         self.chapter.addItems(["1", "2", "3", "4", "5"])
-        self.book.currentIndexChanged.connect(self.chapter_select)
+        # self.book.currentIndexChanged.connect(self.chapter_select)
 
         # creates an instance of QGridLayout which can be used to position widgets within a window
         layout = QGridLayout()
@@ -136,44 +146,25 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.input, 5, 1)
         layout.addWidget(self.search_word, 5, 0)
         layout.addWidget(search, 6, 1)
-        layout.addWidget(self.filler, 6, 0)
-        layout.addWidget(self.stats, 7, 0)
+        layout.addWidget(self.filler, 8, 0)
+        # layout.addWidget(self.stats, 7, 0)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-    def version_select(self, i):
-        print("Items in the list are: ")
-
-        for count in range(self.version.count()):
-            print(self.version.itemText(count))
-        print(f"Current index {i} selection changed {self.version.currentText()}")
-
-    def book_select(self, i):
-        print("Items in the list are: ")
-
-        for count in range(self.book.count()):
-            print(self.book.itemText(count))
-        print(f"Current index {i} selection changed {self.book.currentText()}")
-
-    def chapter_select(self, i):
-        print("Items in the list are: ")
-
-        for count in range(self.chapter.count()):
-            print(self.chapter.itemText(count))
-        print(f"Current index {i} selection changed {self.chapter.currentText()}")
-
     def search_bible(self):
-        # this function should scrape the bible.com website for the specified text
-        x = requests.get('https://www.bible.com/bible/111/GEN.1.NIV')
-        num_times = 0
+        # this function should scrape the website for the specified text
+        x = requests.get(
+            f"https://www.biblegateway.com/passage/?search={self.book.currentText()}+{self.chapter.currentText()}&version={self.version.currentText()}")
         soup = BeautifulSoup(x.content, "html.parser")
-        print(soup)
-        for i in soup:
-            if self.input.text() == i:
-                num_times += 1
-        print(num_times)
+        text = ""
+        for i in soup.find_all("p"):
+            text += i.get_text()
+
+        # counts the number of occurrences of given search word
+        self.num_times = text.upper().count(self.input.text().upper(), 0, (text.find("Holy Bible", 0, len(text))))
+        self.filler.setText(f"Number of times the word appears: {self.num_times}")
 
 
 app = QApplication(sys.argv)
@@ -182,3 +173,6 @@ window = MainWindow()
 window.show()
 
 app.exec()
+
+# TODO: Bugslist:
+# currently increments if part of the word is present eg: good in goodbye
