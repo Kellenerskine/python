171cfba121306287e21f49d1b9c2d98b7829954e
Kellenerskine <kellenae129@gmail.com>
2022-10-18 01:04:51 +0800

    workingonit

--- misc/katas/move_zeroes.py
+++ misc/katas/move_zeroes.py
@@ -1,2 +1,16 @@
 def move_zeros(lst):
-    return lst
+    listy = list(lst)
+    counter = 0
+    for i in listy:
+        if i == 0:
+            listy.pop(i)
+            counter += 1
+
+    for i in range(0, counter):
+        listy.append(0)
+
+    return listy
+
+
+free = move_zeros([1, 0, 1, 2, 0, 1, 3])
+print(free)
